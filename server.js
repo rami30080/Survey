6b1d8052373a5d4f94a17cc8d0cfbd52886acdb5
Rami <ramiayoub12123@gmail.com>
2021-11-02 13:58:51 +0200

    procfile

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const app = express(); //Line 2
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000; //Line 3
 
-this.app.use(express.static(
-  path.join(__dirname,"./client/build")));
-
 
 const url = "mongodb+srv://rami30080:r%40m%21Ay30090@cluster0.cyu3f.mongodb.net/test";
 
